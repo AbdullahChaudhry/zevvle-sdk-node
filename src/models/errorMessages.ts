9d140958f677bb92679c5f800e0028d97ea773df
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2021-02-08 23:38:57 +0000

    add error messages

---
+++ src/models/errorMessages.ts
@@ -0,0 +1,12 @@
+export const ErrorMessages = {
+  apiKeyError: 'Missing API key.',
+  nonGeoNumberError: 'Missing phone number.',
+  accountIdError: 'Missing account id.',
+  chargeIdError: 'Missing charge id.',
+  userIdError: 'Missing user id.',
+  simIdError: 'Missing sim id',
+  callRecordIdError: 'Missing call record id.',
+  urlError: 'Missing url.',
+  webhookIdError: 'Missing webhook id.',
+  webhookUrlError: 'Missing webhook url.'
+}
